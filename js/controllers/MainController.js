4e73e5cd4e57e059eebf939adf9f25a5747e73c9
Porscha07 <porscha.uga@gmail.com>
2017-11-01 22:20:39 -0400

    codecademy cont filters directives controllers

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,44 @@
-app.controller('MainController', function($scope) { 
-  $scope.title = 'This Month\'s Bestsellers'; 
-  $scope.promo = 'The most popular books this month.';
-});
+app.controller('MainController', ['$scope', function($scope) { 
+  $scope.title = 'This is a test'; 
+  $scope.promo = 'I cant believe I am actually learning Angular.';
+   $scope.products = [ 
+  { 
+    name: 'The Book of Trees', 
+    price: 19, 
+    pubdate: new Date('2014', '03', '08'), 
+    cover: 'img/the-book-of-trees.jpg',
+    likes: 0,
+    dislikes: 0
+  }, 
+  { 
+    name: 'Program or be Programmed', 
+    price: 8, 
+    pubdate: new Date('2013', '08', '01'), 
+    cover: 'img/program-or-be-programmed.jpg' ,
+    likes: 0,
+    dislikes:0
+  },
+       { 
+    name: 'To Kill a Mockingbird', 
+    price: 13, 
+    pubdate: new Date('2015', '08', '02'), 
+    cover: 'img/program-or-be-programmed.jpg',
+    likes: 0,
+    dislikes: 0 
+  },
+     { 
+    name: 'The Alchemist', 
+    price: 20, 
+    pubdate: new Date('2017', '10', '01'), 
+    cover: 'img/program-or-be-programmed.jpg',
+    likes: 0,
+    dislikes:0
+  }
+]
+ $scope.plusOne = function(index) {
+    $scope.products[index].likes += 1;
+  };
+   $scope.minusOne = function(index) {
+    $scope.products[index].dislikes += 1;
+  };
+}]);
